14d762345b2fd50069c42a84ff9707393a99ee55
ehvi8r <48869446+ehvi8r@users.noreply.github.com>
2022-03-31 13:21:20 -0600

    date changed

--- frontend/js/countdown.js
+++ frontend/js/countdown.js
@@ -1,7 +1,7 @@
 //Countdown Timer
 const clockdiv = document.getElementById("countdown");
 const countDownTime = new Date(
-  clockdiv.getAttribute("data-date")
+  clockdiv.getAttribute("Apr 16 2022 08:00:45 PM UTC")
 ).getTime();
 
 const countdownfunction = setInterval(function () {
